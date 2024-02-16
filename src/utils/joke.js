6ecbe7b572b62d0db7f366e31e1cc355b446a92b
albretsen <bjellanda@gmail.com>
2024-02-16 11:58:56 +0100

    Added contestId to all Joke entries

--- src/utils/joke.js
+++ src/utils/joke.js
@@ -1,7 +1,17 @@
+import ContestService from "../services/contest.js";
+
 export const GenerateJokeJSON = async (userID, textBody) => {
+    let contest = await ContestService.findByCriteria({ date: new Date() });
+
+    if (!contest[0].id) {
+        throw new Error("No corresponding contest could be found");
+    }
+
     return {
         textBody: textBody,
         userId: userID,
         createTimeStamp: new Date(),
+        contestId: contest[0].id,
+        score: 0,
     }
 }
