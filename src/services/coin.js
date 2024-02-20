13657eda670c7ca2b97e13ffb90031a78da7ba3a
albretsen <bjellanda@gmail.com>
2024-02-20 20:32:24 +0100

    Added purchase function to CoinService

--- src/services/coin.js
+++ src/services/coin.js
@@ -2,49 +2,68 @@ import { Coin } from "../models/init.js";
 import DatabaseError from "../models/error.js";
 
 class CoinService {
-  static async list() {
-    try {
-      return Coin.findMany();
-    } catch (err) {
-      throw new DatabaseError(err);
-    }
-  }
+    static async purchase(userId, amountToDecrement) {
+        try {
+            const coin = await CoinService.get(userId);
+            if (!coin) {
+                throw new Error('Coin not found');
+            }
 
-  static async get(userId) {
-    try {
-      return await Coin.findUnique({ where: { userId } });
-    } catch (err) {
-      throw new DatabaseError(err);
-    }
-  }
+            if (coin.amount < amountToDecrement) {
+                throw new Error('Insufficient coin amount');
+            }
 
-  static async create(data) {
-    try {
-      return await Coin.create({ data });
-    } catch (err) {
-      throw new DatabaseError(err);
-    }
-  }
+            const newAmount = coin.amount - amountToDecrement;
 
-  static async update(id, data) {
-    try {
-      return await Coin.update({
-        where: { id },
-        data,
-      });
-    } catch (err) {
-      throw new DatabaseError(err);
+            return await CoinService.update(userId, { amount: newAmount });
+        } catch (err) {
+            throw new DatabaseError(err);
+        }
     }
-  }
 
-  static async delete(id) {
-    try {
-      await Coin.delete({ where: { id } });
-      return true;
-    } catch (err) {
-      throw new DatabaseError(err);
+    static async list() {
+        try {
+            return Coin.findMany();
+        } catch (err) {
+            throw new DatabaseError(err);
+        }
     }
-  }
+
+    static async get(userId) {
+        try {
+            return await Coin.findUnique({ where: { userId } });
+        } catch (err) {
+            throw new DatabaseError(err);
+        }
+    }
+
+    static async create(data) {
+        try {
+            return await Coin.create({ data });
+        } catch (err) {
+            throw new DatabaseError(err);
+        }
+    }
+
+    static async update(id, data) {
+        try {
+            return await Coin.update({
+                where: { id },
+                data,
+            });
+        } catch (err) {
+            throw new DatabaseError(err);
+        }
+    }
+
+    static async delete(id) {
+        try {
+            await Coin.delete({ where: { id } });
+            return true;
+        } catch (err) {
+            throw new DatabaseError(err);
+        }
+    }
 }
 
 export default CoinService;
