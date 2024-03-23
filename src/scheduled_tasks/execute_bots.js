df05d0951a974ec44457564ddbd006cc1aa00fb7
albretsen <bjellanda@gmail.com>
2024-03-23 12:31:45 +0100

    Increased bot randomization

--- src/scheduled_tasks/execute_bots.js
+++ src/scheduled_tasks/execute_bots.js
@@ -1,10 +1,8 @@
 import cron from 'node-cron';
 import { ExecuteBots } from '../utils/bot.js'; 
-//import NotificationService from '../services/notification.js';
 
 export let ScheduleExecuteBots = () => {
     try {
-        //NotificationService.sendNotifications([{ userId: 29, title: "Title goes here", body: "Message goes here" }])
         ExecuteBots();
     } catch (err) {
         console.log("Error executing bots: " + err);
--- src/utils/bot.js
+++ src/utils/bot.js
@@ -2,9 +2,12 @@ import axios from 'axios';
 import ContestService from "../services/contest.js"
 import JokeDatasetService from "../services/joke_dataset.js";
 
-let TOKENS = ["zdrCqhzf1j8Eg36ShhX79j7ta.YBCv848SElDO9A8GpO7aAGiexeVhSFgrKJw5ls",
-    "QRaJ3oP.3okgWsu8LT4UGS.yn8kGe9u42psfXYcuqy8a719C8GevdkdM4kNH9Twb",
-    "jZWkZdTLW5zuPGn1CY7zF6DDluCSHXCe.WMGJ7u.N7jfdUEr2IsYciZXglzMJ28Q"]
+let TOKENS = ["646vcMdcjQSYnEM87Qp.Ig8Nd047H1QX4PJqOBBWM.cWAXJCg.8gQwWWLxAOtSd9",
+    "Dtyc5ajo2CdppqLQyY.VVTLzz6nIdd52tB.YU3HNTdXf5NrDQ7eYegrezFMy0LT8",
+    "uTE6iChChnNagt9bqnoJMQpl87Rny3oehWIL9CtNp7CpNQ7l8mUMbGWu.GwPx5t0",
+    "0XSadAIszhhCeDbPXifsoIJS3Om59aivj4kD6EDWq3Qq2TI18XO9PRpwz0prnqtD",
+    "jZWkZdTLW5zuPGn1CY7zF6DDluCSHXCe.WMGJ7u.N7jfdUEr2IsYciZXglzMJ28Q",
+    "QRaJ3oP.3okgWsu8LT4UGS.yn8kGe9u42psfXYcuqy8a719C8GevdkdM4kNH9Twb"]
 
 export const ExecuteBots = async () => {
     const contest = await ContestService.getCurrentContest();
@@ -13,8 +16,11 @@ export const ExecuteBots = async () => {
     const jokes = await JokeDatasetService.findByCriteria({ category: contest.topic });
 
     if (jokes.length > 0) {
-        for (const token of TOKENS) {
-            const jokesToPost = jokes.sort(() => 0.5 - Math.random()).slice(0, Math.floor(Math.random() * 2) + 2);
+        const numberOfBots = Math.floor(Math.random() * (TOKENS.length - 4 + 1)) + 4;
+        const randomizedTokens = TOKENS.sort(() => 0.5 - Math.random()).slice(0, numberOfBots);
+        for (const token of randomizedTokens) {
+            const numberOfJokesToPost = Math.random() > 0.5 ? 1 : 2;
+            const jokesToPost = jokes.sort(() => 0.5 - Math.random()).slice(0, numberOfJokesToPost);
             for (const joke of jokesToPost) {
                 await postJoke(joke, token);
             }
